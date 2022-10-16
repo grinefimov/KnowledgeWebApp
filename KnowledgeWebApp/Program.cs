f45510d27f0834cdf8e27a0fdeedae43d9bbc272
Grigorii Efimov <v-gefimov@ebsco.com>
2022-10-17 01:05:23 +0500

    Add React with TypeScript

--- KnowledgeWebApp/Program.cs
+++ KnowledgeWebApp/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html"); 
 
 app.Run();
